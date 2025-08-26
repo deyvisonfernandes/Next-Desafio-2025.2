6b2a3063269451761b9b9d455e511b45c0b42c45
Deyvison Fernandes <deyvisonotaviano@gmail.com>
2025-08-26 15:22:41 -0300

    Configura o globals.css e o layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
 
 export default function Home() {
   return (
-    <></>
+    <>
+
+    <div className="flex flex-col items-center justify-center h-screen bg-amber-700 sm:bg-amber-200 md:bg-amber-600">
+      <div className="border-2 border-blue-900 p-9 text-shadow-amber-500 bg-blanco ">
+        <h1 className=" font-saira font-semibold text-2xl">Bem vindo ao meu primeiro projeto!</h1>
+      </div>
+      
+    </div>
+    
+    
+    
+    </>
+    
   );
 }
